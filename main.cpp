9b6d4358525b36c31084549b8e0f7bf30fac0d80
waliszak2 <waliszak2@gmail.com>
2014-03-26 10:59:34 +0100

    dodanie cpp

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class cOsoba
+{
+public:
+
+	string imie;
+	string nazwisko;
+	int wiek;
+
+
+
+	cOsoba();
+	~cOsoba();
+	void wypisz();
+
+};
+
+cOsoba::cOsoba()
+{
+	cout<<"konstruktor"<<endl;
+	imie="Piotr";
+	nazwisko="Be";
+	wiek=22;
+}
+
+cOsoba::~cOsoba()
+{
+	cout<<"destruktor"<<endl;
+	system("pause");
+
+}
+
+
+void cOsoba::wypisz ()
+{
+	cout<<imie<<" "<<nazwisko<<" "<<wiek<<endl;
+}
+
+
+int main()
+{
+	cOsoba o1;
+	system("pause");
+	o1.wypisz();
+
+
+	system("pause");
+	return 0;
+}
